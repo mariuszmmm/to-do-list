fcd0f3157b8f3384261058c594629c4008d713fc
Mariusz Matusiewicz <mariuszmmm@op.pl>
2022-11-18 15:43:26 +0100

    convert flex to grid

--- js/script.js
+++ js/script.js
@@ -1,82 +1,85 @@
 {
-  const tasks = [];
+	const tasks = [];
 
-  const addNewTas = (newTaskContent) => {
-    tasks.push({
-      content: newTaskContent,
-    });
-    render();
-  };
+	const addNewTas = (newTaskContent) => {
+		tasks.push({
+			content: newTaskContent,
+		});
+		render();
+	};
 
-  const removeTask = (taskIndex) => {
-    tasks.splice(taskIndex, 1);
-    render();
-  };
+	const removeTask = (taskIndex) => {
+		tasks.splice(taskIndex, 1);
+		render();
+	};
 
-  const toggleTaskDone = (taskIndex) => {
-    tasks[taskIndex].done = !tasks[taskIndex].done;
-    render();
-  };
+	const toggleTaskDone = (taskIndex) => {
+		tasks[taskIndex].done = !tasks[taskIndex].done;
+		render();
+	};
 
-  const bindEvents = () => {
-    const removeButtons = document.querySelectorAll(".js-remove");
+	const bindEvents = () => {
+		const removeButtons = document.querySelectorAll(".js-remove");
 
-    removeButtons.forEach((removeButton, index) => {
-      removeButton.addEventListener("click", () => {
-        removeTask(index);
-      });
-    });
+		removeButtons.forEach((removeButton, index) => {
+			removeButton.addEventListener("click", () => {
+				removeTask(index);
+			});
+		});
 
-    const toggleDoneButtons = document.querySelectorAll(".js-done");
+		const toggleDoneButtons = document.querySelectorAll(".js-toggleDone");
 
-    toggleDoneButtons.forEach((toggleDoneButton, index) => {
-      toggleDoneButton.addEventListener("click", () => {
-        toggleTaskDone(index);
-      });
-    });
-  };
+		toggleDoneButtons.forEach((toggleDoneButton, index) => {
+			toggleDoneButton.addEventListener("click", () => {
+				toggleTaskDone(index);
+			});
+		});
+	};
 
-  const render = () => {
-    let htmlString = "";
+	const render = () => {
+		let htmlString = "";
 
-    for (const task of tasks) {
-      htmlString += `  
-        <li class="form__taskContainer"
-        >
-          <button class="form__doneButton js-done">${task.done ? "🗸" : ""}</button>
-          <p class="form__task${task.done ? " form__task--done\"" : "\""}>${task.content}</p>
-          <button class="form__removeButton js-remove">🗑︎</button>
-        </li>
-      `;
-    };
+		for (const task of tasks) {
+			htmlString += `  
+        	<li class="tasks__item">
+          		<button class="tasks__button tasks__button--toggleDone js-toggleDone">
+          			${task.done ? "🗸" : ""}
+				</button>
+          		<p class="tasks__content${task.done ? " tasks__content--toggleDone\"" : "\""}>
+					${task.content}
+          		</p>
+          		<button class="tasks__button tasks__button--remove js-remove">🗑︎</button>
+        	</li>
+      		`;
+		};
 
-    document.querySelector(".js-tasks").innerHTML = htmlString;
+		document.querySelector(".js-tasks").innerHTML = htmlString;
 
-    bindEvents();
-  };
+		bindEvents();
+	};
 
-  const onFormSubmit = (event) => {
-    event.preventDefault();
+	const onFormSubmit = (event) => {
+		event.preventDefault();
 
-    const newTaskContent = document.querySelector(".js-newTask").value.trim();
-    const newTask = document.querySelector(".js-newTask")
-    console.log("submit", newTaskContent);
-    if (newTaskContent === "") {
-      newTask.focus();
-      return;
-    }
+		const newTaskContent = document.querySelector(".js-newTask").value.trim();
+		const newTask = document.querySelector(".js-newTask")
+		console.log("submit", newTaskContent);
+		if (newTaskContent === "") {
+			newTask.focus();
+			return;
+		}
 
-    addNewTas(newTaskContent);
-    newTask.value = "";
-    newTask.focus();
-  };
+		addNewTas(newTaskContent);
+		newTask.value = "";
+		newTask.focus();
+	};
 
-  const init = () => {
-    render();
+	const init = () => {
+		render();
 
-    const form = document.querySelector(".js-form");
-    form.addEventListener("submit", onFormSubmit);
-  };
-  init();
+		const form = document.querySelector(".js-form");
+		form.addEventListener("submit", onFormSubmit);
+	};
+	init();
 }
 
